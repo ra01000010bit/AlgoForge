2bc6c9aab80819a6f878e373e9c39e882f083314
ra01000010bit <ra01000010bit@gmail.com>
2025-03-20 13:48:10 +0100

    Main update

--- AlgoForge.Main/Program.cs
+++ AlgoForge.Main/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace AlgoForge.AlgoForge.Main
+﻿using System.Text.RegularExpressions;
+
+namespace AlgoForge.AlgoForge.Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Welcome massage + start menu
 
+            Console.WriteLine("Hello");
+
+            Console.WriteLine("Kérlek válassza az alábbi menü pontok közül!");
+            Console.WriteLine("1. Egyszerű programozási tételek.");
+            Console.WriteLine("2. Összetett programozási tételek.");
+            Console.WriteLine("3. Programozási tételek össze építése");
+
+            // int mainAnswer = int.Parse(Console.ReadLine());
+            Console.Clear();
+            #endregion
         }
     }
 }
